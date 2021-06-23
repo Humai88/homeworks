6e4ba3deb6cedb57e84ef804dc01569b48a4a580
Gumay <humaivier88@gmail.com>
2021-06-23 17:31:20 +0100

    Add Error404 page

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import s from "./Error404.module.css";
 
 function Error404() {
-    return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-        </div>
-    )
+  return (
+    <div className={s.wrapper}>
+      <div className={s.errorCode}>404</div>
+      <div className={s.errorText}>There is nothing to do here...</div>
+      <img
+        src="https://www.seekpng.com/png/full/359-3590327_shopify-stores-sad-robot-shopify.png"
+        alt="robot"
+        className={s.errorImg}
+      />
+    </div>
+  );
 }
 
-export default Error404
+export default Error404;
