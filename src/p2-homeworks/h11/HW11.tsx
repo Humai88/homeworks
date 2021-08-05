afe722879e2947d494ba96238c84d9c2d1a2181f
Gumay <humaivier88@gmail.com>
2021-08-05 19:40:13 +0100

    Draft homework 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,52 @@
-import React, {useState} from 'react'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import React, { Fragment, useState } from "react";
+import SuperRange from "./common/c7-SuperRange/SuperRange";
+import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
+import styles from "./HW11.module.css";
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(100);
+  const [disable, setDisable] = useState(false);
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+  const changeHandler = (value: number) => {
+    setValue1(value);
+  };
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+  const changeDoubleHandler = (min: number, max: number) => {
+    setValue1(min);
+    setValue2(max);
+    if (value1 >= value2 || value2 <= value1) {
+      setDisable(true);
+    }
+  };
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+  return (
+    <Fragment>
+      <div className={styles.mainWrapper}>
+        homeworks 11
+        {/*should work (должно работать)*/}
+        <div className={styles.wrapper}>
+          <span className={styles.header}>{value1}</span>
+          <SuperRange onChangeRange={changeHandler} value={value1} />
         </div>
-    )
+        <div className={styles.wrapper}>
+          <div className={styles.header}>
+            <span>{value1}</span>
+            <span>{value2}</span>
+          </div>
+          <div>
+            <SuperDoubleRange
+              onChangeRange={changeDoubleHandler}
+              // min={value1}
+              // max={value2}
+              disabled={disable}
+            />
+          </div>
+        </div>
+      </div>
+      <hr />
+    </Fragment>
+  );
 }
 
-export default HW11
+export default HW11;
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,72 @@
-import React from 'react'
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  useState,
+} from "react";
+import styles from "./SuperDoubleRange.module.css";
 
-type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
-}
+type SuperDoubleRangePropsType = DefaultInputPropsType & {
+  onChangeRange?: (value1: number, value2: number) => void;
+  value?: [number, number];
+  min?: number;
+  max?: number;
+  step?: number;
+  disable?: boolean;
+};
+type NewType = InputHTMLAttributes<HTMLInputElement>;
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
+type DefaultInputPropsType = DetailedHTMLProps<NewType, HTMLInputElement>;
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({
+  onChangeRange,
+  onChange,
+  value,
+  min,
+  max,
+  step,
+  disable,
+  className,
+  ...restProps
+}) => {
+  type InitialValuesType = {
+    startValue: number;
+    endValue: number;
+  };
+  const [values, setValues] = useState<InitialValuesType>({
+    startValue: 0,
+    endValue: 100,
+  }); // Set values for two inputs
 
-    return (
-        <>
-            DoubleRange
-        </>
-    )
-}
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e);
+    let { name, value } = e.currentTarget;
+    let newValue = +value;
+    setValues({
+      ...values,
+      [name]: newValue,
+    });
+    onChangeRange && onChangeRange(values.startValue, values.endValue);
+  };
+  const finalRangeClassName = `${styles.slider} ${className ? className : ""}`;
+  return (
+    <div>
+      <input
+        type="range"
+        className={finalRangeClassName}
+        onChange={onChangeCallback}
+        value={values.endValue}
+        name="endValue"
+      />
+      <input
+        type="range"
+        className={finalRangeClassName}
+        onChange={onChangeCallback}
+        value={values.startValue}
+        name="startValue"
+      />
+      {disable && <h1>Try again</h1>}
+    </div>
+  );
+};
 
-export default SuperDoubleRange
+export default SuperDoubleRange;
