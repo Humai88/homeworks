392b72f03ce8cd90232dd4a458a3e4b47bdf35be
Gumay <humaivier88@gmail.com>
2021-08-19 11:02:12 +0100

    Change nav

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import s from "./Header.module.css";
+import "./Header.css";
 import { FaArrowRight } from "react-icons/fa";
 
 function Header() {
+  const [nav, setNav] = useState(false);
+  const onChangeHandler = () => {
+    setNav(!nav);
+  };
+  const hideNavbar = () => {
+    setNav(false);
+  };
   return (
-    <div className={s.nav}>
+    <div className="menu-wrap">
+      <input
+        type="checkbox"
+        className="toggler"
+        onChange={onChangeHandler}
+        checked={nav}
+      />
+      <div className="hamburger">
+        <div></div>
+      </div>
+      <div className="menu">
+        <div>
+          <div>
+            <ul>
+              <NavLink
+                to="/pre-junior"
+                onClick={hideNavbar}
+                activeClassName="active"
+              >
+                Pre-Junior
+              </NavLink>
+              <NavLink
+                to="/junior"
+                onClick={hideNavbar}
+                activeClassName="active"
+              >
+                Junior
+              </NavLink>
+              <NavLink
+                to="/junior-plus"
+                onClick={hideNavbar}
+                activeClassName="active"
+              >
+                Junior+
+              </NavLink>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+
+{
+  /* <div className={s.nav}>
       <NavLink to="/pre-junior" activeClassName={s.active}>
         Pre-Junior
       </NavLink>
@@ -16,8 +69,5 @@ function Header() {
         Junior+
       </NavLink>
       <FaArrowRight className={s.trigger} />
-    </div>
-  );
+    </div> */
 }
-
-export default Header;
